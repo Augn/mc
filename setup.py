87c848a9ed8fe6d0ad33e88371d2a63e91ee325b
forgetting_time <wang1183478375@outlook.com>
2019-08-08 16:56:17 +0800

    003

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
-    name='face_maker',
-    version='0.0.1',
+    name='mcpython',
+    version='0.0.2',
     author='魔扣少儿编程',
     author_email='',
     url ='https://github.com/Augn/mc',
